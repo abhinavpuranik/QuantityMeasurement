eab2ac1d501e8bdeb87cc2c65020b390fd6e1dd0
abhinavpuranik <abhunav.puranik@gmail.com>
2026-03-31 10:19:39 +0530

    Coded the spring entrypoint

---
+++ src/main/java/com/example/quantitymanagement/DemoApplication.java
@@ -0,0 +1,18 @@
+/*
+ * @author: Developer
+ * version: 1.0
+ */
+
+package com.example.quantitymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
