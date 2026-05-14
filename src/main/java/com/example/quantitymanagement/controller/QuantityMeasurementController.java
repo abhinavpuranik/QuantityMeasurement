1ba55bcce1759e0111cfe877ae7ccbb5176e8b7b
Abhinav Puranik <113786221+abhinavpuranik@users.noreply.github.com>
2026-05-14 11:44:27 +0530

    Change success message from 'success' to 'successful'

--- src/main/java/com/example/quantitymanagement/controller/QuantityMeasurementController.java
+++ src/main/java/com/example/quantitymanagement/controller/QuantityMeasurementController.java
@@ -64,7 +64,7 @@ public class QuantityMeasurementController {
 
     @GetMapping("/success")
     public String success(){
-        return "success";
+        return "successful";
         
     }
     
